3d09f3a7526d02bcd114a769311797704789a002
IN9 <160686822+POSIO-IN9@users.noreply.github.com>
2024-11-18 08:11:56 +0900

    HashRouter사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {RecoilRoot} from "recoil";
 import Pjmain from "./projectReviews/Pjmain";
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <RecoilRoot>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/project" element={<Pjmain />} />
+          <Route path="/:project" element={<Pjmain />} />
         </Routes>
       </RecoilRoot>
-    </Router>
+    </HashRouter>
   );
 }
 
