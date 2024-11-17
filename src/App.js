c723c6dc71870551fd7133352d0fd2a27c07affd
IN9 <160686822+POSIO-IN9@users.noreply.github.com>
2024-11-18 08:47:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {RecoilRoot} from "recoil";
 import Pjmain from "./projectReviews/Pjmain";
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <RecoilRoot>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path=":/project" element={<Pjmain />} />
         </Routes>
-      </RecoilRoot>
     </Router>
   );
 }
