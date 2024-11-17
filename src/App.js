3bcc167065be5fadbf0289f3a52d44822655acee
IN9 <160686822+POSIO-IN9@users.noreply.github.com>
2024-11-18 08:28:49 +0900

    라우터수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {RecoilRoot} from "recoil";
 import Pjmain from "./projectReviews/Pjmain";
 function App() {
   return (
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <RecoilRoot>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/:project" element={<Pjmain />} />
+          <Route path="/project" element={<Pjmain />} />
         </Routes>
       </RecoilRoot>
-    </HashRouter>
+    </Router>
   );
 }
 
